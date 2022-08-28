ae0eafcc2a0b9031a070c1690af8f05676e9f06d
s3np41k1r1t0 <brunomendes@tecnico.ulisboa.pt>
2022-08-28 22:59:52 +0100

    write async calls properly

--- frontend/src/pages/GraphView.tsx
+++ frontend/src/pages/GraphView.tsx
@@ -1,7 +1,7 @@
 import { Flow, useTulip } from "../api";
 import { FlowGraph } from "../components/FlagGraph";
 import { getTimeStuffFromParams } from "../utils";
-import { Suspense, useRef } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 export function GraphView() {
   const { services, getFlows } = useTulip();
@@ -18,25 +18,43 @@ export function GraphView() {
   
   // FIXME this could be avoided if FlowList saves the Flows internally
   // but maybe we would be limitated by the 2000 flows limit in the backend
-  const flowsIn  = useRef<Flow[]>([]);
-  (async (flowsIn) => 
-    flowsIn.current = await getFlows({
-      service: "", // FIXME
-      from_time: String(tickToUnixTime(start)),
-      to_time: String(tickToUnixTime(end)),
-      tags: ["flag-in"],
-    })
-  )(flowsIn)
+  const [flowsIn, setFlowsIn] = useState<Flow[]>([]);
+  
+  useEffect(() => {
+    let active = true
+    getData()
+    return () => { active = false }
 
-  const flowsOut = useRef<Flow[]>([]);
-  (async (flowsOut) => 
-    flowsOut.current = await getFlows({
-      service: "", // FIXME
-      from_time: String(tickToUnixTime(start)),
-      to_time: String(tickToUnixTime(end)),
-      tags: ["flag-out"],
-    })
-  )(flowsOut)
+    async function getData() { 
+      const res = await getFlows({
+        service: "", // FIXME
+        from_time: String(tickToUnixTime(start)),
+        to_time: String(tickToUnixTime(end)),
+        tags: ["flag-in"],
+      })
+      if (!active) return
+      setFlowsIn(res)
+    }
+  }, [start, end])
+
+  const [flowsOut, setFlowsOut] = useState<Flow[]>([]);
+  
+  useEffect(() => {
+    let active = true
+    getData()
+    return () => { active = false }
+
+    async function getData() { 
+      const res = await getFlows({
+        service: "", // FIXME
+        from_time: String(tickToUnixTime(start)),
+        to_time: String(tickToUnixTime(end)),
+        tags: ["flag-out"],
+      })
+      if (!active) return
+      setFlowsOut(res)
+    }
+  }, [start, end])
     
   const graphs = services.map(service => 
   <Suspense key={service.name}>
@@ -44,8 +62,8 @@ export function GraphView() {
       service={service} 
       ticks={ticks} 
       flows={[
-        flowsIn.current.filter(flow => flow.dst_port === service.port), 
-        flowsOut.current.filter(flow => flow.dst_port === service.port)
+        flowsIn.filter(flow => flow.dst_port === service.port), 
+        flowsOut.filter(flow => flow.dst_port === service.port)
       ]} 
     />
   </Suspense>
